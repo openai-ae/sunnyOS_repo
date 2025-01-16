6d7f215b9d1f44b4e0a8e1800cf58e0c3a779060
itzParsaYC <115403383+ParsaAslaniYC@users.noreply.github.com>
2025-01-16 23:28:56 +0330

    sunny_mirror: Introduce SunnyOS Grunty

---
+++ sunnyos_grunty/report.py
@@ -0,0 +1,6 @@
+def report_error(msg: str):
+    print("########################################")
+    print("# ERROR ERROR ERROR ERROR ERROR ERROR ERROR #")
+    print("########################################")
+    print("\n")
+    print(msg)
